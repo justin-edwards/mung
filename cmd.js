a4c97547dbf67c2682532101bbb6ac9688d376b9
justin-edwards <jedwards@gigmasters.com>
2013-10-02 00:11:07 -0400

    added replacement flag for either static replacement or 'random' to generate a random byte

--- cmd.js
+++ cmd.js
@@ -5,7 +5,8 @@ var mung = require('./mung')
 var argv = require('optimist')
   .alias('i', 'input')
   .alias('s', 'skip').default('s', 0)
+  .alias('r', 'replacement').default('r',65)
   .argv
 
-mung(argv.input, process.stdout, argv.skip)
+mung(argv.input, process.stdout, argv.skip, argv.replacement)
 
--- mung.js
+++ mung.js
@@ -4,15 +4,19 @@ function shouldMung (i) {
   return (i % 500 === 0)
 }
 
-function mungBuf (buf) {
-  return new Buffer([65])
+function mungBuf (buf, replacement) {
+  var rpl = replacement;
+  if(rpl == 'random'){
+	rpl = Math.floor(Math.random() * 256);
+  }
+  return new Buffer([rpl])
 }
 
-var mung = module.exports = function (path, outStream, skip) {
+var mung = module.exports = function (path, outStream, skip, replacement) {
   var buf = fs.readFileSync(path)
   for (var i = 0; i < buf.length; i++) {
     if (i >= skip && shouldMung(i)) {
-      outStream.write(mungBuf(buf[i])) 
+      outStream.write(mungBuf(buf[i], replacement))
     } else {
       outStream.write(new Buffer([buf[i]]))
     }
